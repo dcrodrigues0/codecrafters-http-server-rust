fce7e75e7717b936aa76a03e2c3d0c8c3181dbb4
Daniel <danielrcavalcante@gmail.com>
2024-10-16 12:32:11 -0300

    [respond with 200]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::io::{BufRead, BufReader};
 use std::{io::Write, net::TcpStream};
-#[allow(unused_imports)]
 use std::net::TcpListener;
+
+
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -20,16 +22,46 @@ fn main() {
     }
 }
 
-fn handle_result(mut stream: TcpStream){
+fn handle_result(stream: TcpStream){
+    let tmp_stream = stream.try_clone().unwrap();
+
+    let buff = 
+        BufReader::new(stream).lines().next().unwrap();
+
+    match buff {
+        Ok(str) =>{
+            let mut header_info = str.split_whitespace();
+            header_info.next();
+            let request_path = header_info.next().unwrap();
+
+            match request_path {
+                "/" => {
+                    write_result(tmp_stream, b"HTTP/1.1 200 OK\r\n\r\n");
+                }
+                _ => {
+                    write_result(tmp_stream, b"HTTP/1.1 404 Not Found\r\n\r\n");
+                }
+            }
+            println!("My URL Path: {:?}", str);
+            
+        }
+        Err(err ) => {
+            println!("Error {}", err);
+        }
+    }
+
+}
+
+fn write_result(mut stream:TcpStream, string_buffer: &[u8]){
     let write_result 
-        = stream.write(b"HTTP/1.1 200 OK\r\n\r\n");
-    
+        = stream.write(string_buffer);
+        
     match write_result {
         Ok(_stream ) =>{
             println!("Message sent successfully");
         }
-        Err(e) =>{
-            println!("Cannot send message {}", e);
+        Err(err) =>{
+            println!("Cannot send message {}", err);
         }
     }
 }
