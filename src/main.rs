bffef0d92e6c7be18ec6e482422fe89253aac52f
Daniel <danielrcavalcante@gmail.com>
2024-10-30 22:09:14 -0300

    finally getting headers and req props correctly

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::hash::Hash;
 use std::io::{BufRead, BufReader, Read};
+use std::iter::Peekable;
 use std::str::SplitWhitespace;
 use std::time::Duration;
 use std::usize;
@@ -16,7 +17,8 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                stream.set_read_timeout(Some(Duration::new(0, 1000))).expect("set_read_timeout call failed");
+                stream.set_read_timeout(Some(Duration::new(0, 1000)))
+                    .expect("set_read_timeout call failed");
                 handle_result(stream);
                 println!("accepted new connection");
             }
@@ -29,10 +31,12 @@ fn main() {
 
 fn handle_result(mut stream: TcpStream){
     let mut buf:String = String::new();
-    stream.read_to_string(&mut buf);
+    match stream.read_to_string(&mut buf) {
+        Ok(_) => return,
+        Err(e) => println!("Stream read error: {:?}",e)
+    }
 
     let mut req_lines: SplitWhitespace<'_> = buf.split_whitespace();
-    
     let req_method: &str = match req_lines.nth(0) {
         Some(line) => line,
         None => return
@@ -46,10 +50,10 @@ fn handle_result(mut stream: TcpStream){
         None => return
     };
     
-    //TODO Undertand why this shit is not taking the headers as should grrrrrr Maybe any is already consuming 1th of my lines
+    let mut peek_req_lines: Peekable<_> = req_lines.peekable();
     let mut headers: HashMap<&str, &str> = HashMap::new();
-    while req_lines.any(|line| !line.is_empty()){
-        headers.insert(req_lines.nth(0).unwrap(), req_lines.nth(0).unwrap());
+    while peek_req_lines.peek().is_some() {       
+        headers.insert(peek_req_lines.next().unwrap_or(""), peek_req_lines.next().unwrap_or(""));
     }
     
     println!("Req method: {:?} Req_target: {:?} Http_version: {:?}", req_method, req_target, http_version);
